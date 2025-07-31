b5ad358fb0d8b094fb37f6bde8765b36450aa35c
Aroweeri <dylanlsutton@gmail.com>
2025-07-31 18:30:26 -0400

    Disable too much logging.

--- scripts/rotator.gd
+++ scripts/rotator.gd
@@ -8,5 +8,5 @@ func _process(_delta):
 func _physics_process(delta: float) -> void:
 	var distance_to_player = self.global_position.distance_to($Player.global_position)
 	var rotate_amount = ROTATION_SPEED * (1/distance_to_player) * delta
-	print("Distance: %0.2f Degrees/Sec: %0.2f" % [distance_to_player, rad_to_deg(rotate_amount)/delta])
+	#print("Distance: %0.2f Degrees/Sec: %0.2f" % [distance_to_player, rad_to_deg(rotate_amount)/delta])
 	rotate(rotate_amount)
