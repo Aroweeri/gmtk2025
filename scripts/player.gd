3ae9c0145bfbd670d97bf84396ad90f727707b64
Aroweeri <dylanlsutton@gmail.com>
2025-07-31 18:18:22 -0400

    Speed changes based on distance to center.

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,3 +2,9 @@ extends Node2D
 
 func _process(delta: float) -> void:
 	$Sprite.global_rotation = 0
+	
+func _physics_process(delta: float) -> void:
+	if(Input.is_action_pressed("ui_up")):
+		translate(Vector2(10,0));
+	elif(Input.is_action_pressed("ui_down")):
+		translate(Vector2(-10,0));
--- scripts/rotator.gd
+++ scripts/rotator.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
-var ROTATION_SPEED=1
+var ROTATION_SPEED=500
 
 func _process(_delta):
 	pass;
 	
 func _physics_process(delta: float) -> void:
-	rotate(ROTATION_SPEED * delta)
+	var distance_to_player = self.global_position.distance_to($Player.global_position)
+	var rotate_amount = ROTATION_SPEED * (1/distance_to_player) * delta
+	print("Distance: %0.2f Degrees/Sec: %0.2f" % [distance_to_player, rad_to_deg(rotate_amount)/delta])
+	rotate(rotate_amount)
