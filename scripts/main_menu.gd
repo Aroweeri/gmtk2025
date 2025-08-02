ba42bd71238db5ea3c83f03322599b86412ec67c
Aroweeri <dylanlsutton@gmail.com>
2025-08-01 21:47:07 -0400

    Add Main Menu.

---
+++ scripts/main_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Main.tscn")
+
+
+func _on_options_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_credits_button_pressed() -> void:
+	pass
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit();
